ae214ab0b4abe251f6eefa1dc8acb8b76db964ff
TheSalarKhan <muhammedsalarkhan@gmail.com>
2024-06-02 06:11:35 +0500

    get two peers connected

--- examples/zen-rtc-html-app/src/index.ts
+++ examples/zen-rtc-html-app/src/index.ts
@@ -1,22 +1,54 @@
 import { SimplePeer } from '@zen-rtc/index';
 
-async function main() {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
-  (window as any).DEBUG='*';
-  // const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+function main() {
+
+  const rtcConfiguration: RTCConfiguration = {
+    iceServers: [
+      {
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:global.stun.twilio.com:3478'
+        ]
+      }
+    ],
+  };
+
   const peer1 = new SimplePeer({
+    config: rtcConfiguration,
     initiator: true,
     enableLogging: true,
     wrtc: {
       RTCIceCandidate,
       RTCPeerConnection,
       RTCSessionDescription,
-    }
+    },
   });
-  console.log(peer1);
-  console.log('heya');
+
+  const peer2 = new SimplePeer({
+    config: rtcConfiguration,
+    initiator: false,
+    enableLogging: true,
+    wrtc: {
+      RTCIceCandidate,
+      RTCPeerConnection,
+      RTCSessionDescription,
+    },
+  });
+
+  peer1.on('connect', () => {
+    console.log('peer1 connected');
+  });
+  peer1.on('signal', (payload) => {
+    peer2.signal(payload);
+  });
+  peer2.on('connect', () => {
+    console.log('peer2 connected');
+  });
+  peer2.on('signal', (payload) => {
+    peer1.signal(payload);
+  });
 }
 
 window.addEventListener('load', () => {
-  main().then(() => {}).catch(() => {});
+  main();
 });
